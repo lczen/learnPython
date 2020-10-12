b3939fbbcae30f0ac8e6542d4da4933ce6afee28
lczen <729188109@qq.com>
2020-10-12 16:37:20 +0800

    回文子串 && 划分子区间

---
+++ 动态规划/青蛙跳有两个方向(up&right).py
@@ -0,0 +1,16 @@
+N = 6
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+dp[0][1] = 1
+dp[1][0] = 1
+dp[1][1] = 2
+dp[0][2] = 2
+dp[2][0] = 2
+for i in range(2, len(dp)):
+    dp[0][i] = dp[0][i - 1] + dp[0][i - 2]
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+
+for i in range(2, len(dp)):
+    for j in range(2, len(dp[0])):
+        dp[i][j] = dp[i - 2][j] + dp[i - 1][j] + dp[i][j - 2] + dp[i][j - 1]
+print(dp[-1][-1])
